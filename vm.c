d8b0e930a04dfd4de6e218a2f981dbdc87f83573
Danny <dannyquinn7@me.com>
2024-12-25 12:30:07 +0000

    basic math

--- vm.c
+++ vm.c
@@ -31,6 +31,12 @@ Value pop() {
 static InterpretResult run() {
     #define READ_BYTE() (*vm.ip++)
     #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+    #define BINARY_OP(op) \
+        do { \
+            double b = pop(); \
+            double a = pop(); \
+            push(a op b); \
+        } while(false)
 
     for (;;) {
     #ifdef DEBUG_TRACE_EXECUTION 
@@ -46,19 +52,25 @@ static InterpretResult run() {
 
         uint8_t instruction; 
         switch (instruction = READ_BYTE()) {
+            case OP_ADD: BINARY_OP(+); break; 
             case OP_CONSTANT: {
                 Value constant = READ_CONSTANT();
                 push(constant);
                 break;
             }
+            case OP_DIVIDE: BINARY_OP(/); break; 
+            case OP_MULTIPLY: BINARY_OP(*); break; 
+            case OP_NEGATE: push(-pop()); break;
             case OP_RETURN:{
                 printValue(pop());
                 printf("\n");
                 return INTERPRET_OK;
             }
+            case OP_SUBTRACT: BINARY_OP(-); break;
         }
     }
 
+    #undef BINARY_OP
     #undef READ_CONSTANT
     #undef READ_BYTE
 }
