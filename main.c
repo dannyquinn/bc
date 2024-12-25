054d2bfa612647a8c3c5b2de323b46ed1e9aa6c6
Danny <dannyquinn7@me.com>
2024-12-25 12:19:49 +0000

    vm

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+    initVM();
     Chunk chunk; 
     initChunk(&chunk);
 
@@ -14,6 +16,9 @@ int main(int argc, const char* argv[]) {
 
     disassembleChunk(&chunk,"test chunk");
 
+    interpret(&chunk);
+
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
